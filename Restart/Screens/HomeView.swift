17f9c253008409369734f9187e7a35d0c83b5c4a
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2023-11-26 13:53:00 +0200

    Handle Home View

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -9,16 +9,43 @@ import SwiftUI
 
 struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
-
+    
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = true
-            }, label: {
-                Text("Restart")
-            })
+
+            Spacer()
+            //: MARK: HEADER
+            ZStack {
+                CircleGroup(shapeColor: .gray, shapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            
+            //: MARK: Centerr
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            Spacer()
+            //: MARK: FOOTER
+            ZStack {
+                Button {
+                    isOnboardingViewActive = true
+                } label: {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .imageScale(.large)
+                    Text("Restart")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }.buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.capsule)
+                    .controlSize(.large)
+               
+            }
         } //: VStack End
     }
 }
